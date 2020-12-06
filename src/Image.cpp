209a1b986fc414f7f15ec188af9b10b867943166
Eliad Karni <EliadK995@Gmail.com>
2020-12-06 13:28:07 +0200

    + operator fix

--- src/Image.cpp
+++ src/Image.cpp
@@ -1,27 +1,30 @@
+//============================ include section ===============================
 #include "Image.h"
 #include "Pixel.h"
 #include <iostream>
 #include <cmath>
-//========================================================================
+
+//========================== constractors section ============================
 Image::Image():m_imageData(ImageDataStracture(0, 0, WHITE)) {}
-//========================================================================
+//============================================================================
 Image::Image(int height, int width) : 
 	m_imageData(ImageDataStracture(height, width, WHITE)){}
-//========================================================================
+//============================================================================
 Image::Image(int height, int width, unsigned char pixel)
 	: m_imageData(ImageDataStracture(height, width, pixel)) {}
-//========================================================================
+//============================================================================
 Image::Image(const Image& other) {
 	*this = other;
 } //copy constructor
-//========================================================================
+//=========================== destructors section ============================
 Image::~Image(){
 	this->m_imageData.~ImageDataStracture();
 }
-//============================================================================
+//============================== get sections ================================
 unsigned int Image::getHeight()const { return this->m_imageData.getHeight(); }
+//============================================================================
 unsigned int Image::getWidth()const { return this->m_imageData.getWidth(); }
-//========================================================================
+//======================== public operators section ==========================
 void Image::operator=(const Image& other) {
 	this->m_imageData.~ImageDataStracture();
 	this->m_imageData =
@@ -30,23 +33,23 @@ void Image::operator=(const Image& other) {
 		for (unsigned int j = 0; j < other.getWidth(); ++j)
 			(*this)(i, j) = other(i, j);
 }
-//========================================================================
+//============================================================================
 void Image::operator|=(const Image& other) {
 	*this = *this | other;
 }
-//========================================================================
+//============================================================================
 void Image::operator&=(const Image& other) {
 	*this = *this & other;
 }
-//========================================================================
+//============================================================================
 Pixel& Image::operator()(unsigned int x, unsigned int y) {
 	return this->m_imageData(x, y);
 }
-//========================================================================
+//============================================================================
 const Pixel& Image::operator()(unsigned int x, unsigned int y)const {
 	return this->m_imageData(x,y);
 }
-//========================================================================
+//======================== global operators section ==========================
 std::ostream& operator<<(std::ostream& os, const Image& A) {
 	for (unsigned int i = 0; i < A.getHeight(); i++) {
 		for (unsigned int j = 0; j < A.getWidth(); j++)
@@ -55,7 +58,7 @@ std::ostream& operator<<(std::ostream& os, const Image& A) {
 	}
 	return os;
 }
-//========================================================================
+//============================================================================
 bool operator==(const Image& A, const Image& B) {
 	if (A.getHeight() != B.getHeight() ||
 		A.getWidth() != B.getWidth())
@@ -66,11 +69,11 @@ bool operator==(const Image& A, const Image& B) {
 				return false;
 	return true;
 }
-//========================================================================
+//============================================================================
 bool operator!=(const Image& A, const Image& B) {
 	return !(A == B);
 }
-//========================================================================
+//============================================================================
 Image operator~(const Image& A) {
 	Image newImage(A.getHeight(), A.getWidth());
 	for (unsigned int i = 0; i < A.getHeight(); i++)
@@ -90,14 +93,14 @@ Image operator~(const Image& A) {
 		}
 	return newImage;
 }
-//========================================================================
+//============================================================================
 void operator+=(Image& A, const Image& B) {
 	A = A + B;
 }
-//========================================================================
+//============================================================================
 Image operator+(const Image& A, const Image& B) {
 	Image ans = Image(
-		A.getHeight() + B.getHeight(),
+		(unsigned int)fmax(A.getHeight(),B.getHeight()),
 		A.getWidth() + B.getWidth(), WHITE);
 
 	for (unsigned int i = 0; i < A.getHeight(); ++i)
@@ -108,14 +111,14 @@ Image operator+(const Image& A, const Image& B) {
 			ans(i, j + A.getWidth()) = B(i, j);
 	return ans;
 }
-//========================================================================
+//============================================================================
 Image operator|(const Image& A, const Image& B) {
 	unsigned int nHeight = (unsigned int)fmax(A.getHeight(), B.getHeight());
 	unsigned int nWidth = (unsigned int)fmax(A.getWidth() , B.getWidth());
 	Image nImage(nHeight, nWidth);
 
 	for(unsigned int i = 0; i < nHeight; ++i)
-		for (unsigned int j; j < nWidth; ++j) {
+		for (unsigned int j = 0; j < nWidth; ++j) {
 			if (A.getHeight() > i && A.getWidth() > j) {
 				if (B.getHeight() > i && B.getWidth() > j)
 					nImage(i, j) = A(i, j) | B(i, j);
@@ -129,7 +132,7 @@ Image operator|(const Image& A, const Image& B) {
 		}
 	return nImage;
 }
-//========================================================================
+//============================================================================
 Image operator&(const Image& A, const Image& B) {
 	unsigned int nHeight = (unsigned int)fmin(A.getHeight(), B.getHeight());
 	unsigned int nWidth = (unsigned int)fmin(A.getWidth(), B.getWidth());
@@ -141,16 +144,16 @@ Image operator&(const Image& A, const Image& B) {
 
 	return nImage;
 }
-//========================================================================
+//============================================================================
 Image operator*(const Image& A, unsigned int n) {
 	Image nImage = Image();
 	for (unsigned int i = 0; i < n; i++)
 		nImage += A;
 	return nImage;
 }
-//========================================================================
+//============================================================================
 Image operator*(unsigned int n, const Image& A) {return (A * n);}
-//========================================================================
+//============================================================================
 void operator*=(Image& A, unsigned int n) {
 	A = A * n;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,27 @@
 #include "iostream"
 #include "Image.h"
 
+using ::std::endl;
 int main()
 {
-	Image image1 = Image(2, 2, WHITE);
-	std::cout << (~image1 + Image(4, 4 , GRAY)) * 3;
+	Image p1(1, 5);
+	Image p2(1, 5, GRAY);
+	Image p3(1, 5, BLACK);
+	std::cout << "                    " << p1 * 7 << endl
+		<< "               " << p1 + p2 + p3 * 5 + p2 + p1 << endl
+		<< "          " << p1 + p3 * 9 + p1 << endl
+		<< "     " << p1 + p2 + p3 * 2 + p1 * 5 + p3 * 2 + p2 + p1 << endl
+		<< p1 + p2 + p3 + p1 * 3 + p3 + p1 + p3 + p1 * 3 + p2 * 2 + p1 << endl
+		<< p1 + p3 * 4 + p1 * 5 + p3 * 2 + p2 * 2 + p1 << endl
+		<< p1 + p3 * 4 + p1 * 5 + p3 * 2 + p2 * 2 + p1 << endl
+		<< p1 + p3 * 4 + p1 * 5 + p3 * 2 + p2 * 2 + p1 << endl
+		<< p1 + p2 + p3 * 4 + p1 * 3 + p3 * 2 + p2 * 3 + p1 << endl
+		<< p1 + p2 + p3 * 9 + p2 * 3 + p1 << endl
+		<< "     " << p1 + p2 + p3 * 7 + p2 * 3 + p1 << endl
+		<< "     " << p1 + p2 * 2 + p3 * 2 + p2 + p3 + p2 * 5 + p1 << endl
+		<< "          " << p1 * 2 + p2 * 7 + p1 * 2 << endl
+		<< "                    " << p1 * 2 + p2 + p1 + p2 + p1 * 2 << endl
+		<< "                         " << p1 + p2 + p1 + p2 + p1 << endl
+		<< "                         " << p1 * 5;
 	return EXIT_SUCCESS;
 }
